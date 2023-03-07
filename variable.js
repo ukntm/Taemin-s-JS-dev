168e4adebc80017864d1b92f8b24a332416806b3
ukntm <127084654+ukntm@users.noreply.github.com>
2023-03-07 20:31:50 +0900

    Day-1

---
+++ variable.js
@@ -0,0 +1,11 @@
+// 변수 
+let name = 'Taemin Kim'
+console.log(name1)
+name = 'Willam'
+console.log(name1)
+
+const name2 = 'Tm2'
+console.log(name2)
+
+// name2 = 'Tm3' -const '상수'는 재할당 불가능 이므로 주석처리
+
